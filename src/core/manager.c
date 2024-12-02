8fb990d95a431dbacb67ca72a33efe14b28ad5d4
turly221 <feifan@scntist.com>
2024-12-02 12:34:07 +0000

    commit patch 4758720

--- src/core/manager.c
+++ src/core/manager.c
@@ -1671,6 +1671,10 @@ static int manager_dispatch_sigchld(Manager *m) {
                 log_debug("Got zero-length notification message. Ignoring.");
                 return 0;
         }
+        if (n == 0) {
+                log_debug("Got zero-length notification message. Ignoring.");
+                return 0;
+        }
 
         return 0;
 }

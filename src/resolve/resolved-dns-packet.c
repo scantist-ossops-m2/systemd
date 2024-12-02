3c8fb55985eac61231ce8bba11896e507330c17e
turly221 <feifan@scntist.com>
2024-12-02 09:39:23 +0000

    commit patch 6009832

--- src/resolve/resolved-dns-packet.c
+++ src/resolve/resolved-dns-packet.c
@@ -1454,7 +1454,7 @@ static int dns_packet_read_type_window(DnsPacket *p, Bitmap **types, size_t *sta
 
                 found = true;
 
-                while (bitmask) {
+                for (; bitmask; bit++, bitmask >>= 1)
                         if (bitmap[i] & bitmask) {
                                 uint16_t n;
 
@@ -1468,10 +1468,6 @@ static int dns_packet_read_type_window(DnsPacket *p, Bitmap **types, size_t *sta
                                 if (r < 0)
                                         return r;
                         }
-
-                        bit++;
-                        bitmask >>= 1;
-                }
         }
 
         if (!found)
